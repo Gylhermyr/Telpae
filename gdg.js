f5829d631547028eab323e0220c530da955539b1
Gylhermyr <jguimouraaraujo2009@gmail.com>
2026-05-04 07:42:46 -0300

    Add files via upload

---
+++ gdg.js
@@ -0,0 +1,15 @@
+function irparacontatos(){
+    document.getElementById("footdiv").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+function irparagruta(){
+    document.getElementById("r2div").scrollIntoView({
+        behavior: "smooth"
+    })
+}
+function irparasobre(){
+    document.getElementById("bdiv").scrollIntoView({
+        behavior: "smooth"
+    })
+}
